b17843c9aaf8676cc53bbf89f8dbaf7fcc4e97e9
六翁 <lu.hl@alibaba-inc.com>
2021-01-30 23:29:25 +0800

    cm wasm

--- propaganda-filter/sh/istio/4-envoy.sh
+++ propaganda-filter/sh/istio/4-envoy.sh
@@ -9,3 +9,4 @@ timestamp=$(date "+%Y%m%d-%H%M%S")
 hello1_v2_pod=$(k get pod -l app=hello1-deploy-v2 -n http-hello -o jsonpath={.items..metadata.name})
 k -n http-hello exec "$hello1_v2_pod" -c istio-proxy \
 -- curl -s "http://localhost:15000/config_dump?resource=dynamic_listeners" >dynamic_listeners-"$timestamp".json
+grep "propaganda-filter.wasm" dynamic_listeners-"$timestamp".json
