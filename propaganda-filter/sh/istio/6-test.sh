ae0dfe8952cfea8cf4767996f0771730a2a81f7d
六翁 <lu.hl@alibaba-inc.com>
2021-01-31 15:15:51 +0800

    cm wasm

--- propaganda-filter/sh/istio/6-test.sh
+++ propaganda-filter/sh/istio/6-test.sh
@@ -15,23 +15,25 @@ curl -H "version:v2" "http://$ingressGatewayIp:8001/hello/eric"
 echo
 curl -H "version:v2" "http://$ingressGatewayIp:8001/hello/eric"
 
-hello3_v2_pod=$(k get pod -l app=hello3-deploy-v2 -n http-hello -o jsonpath={.items..metadata.name})
+mesh_inside_test() {
+  hello3_v2_pod=$(k get pod -l app=hello3-deploy-v2 -n http-hello -o jsonpath={.items..metadata.name})
 
-echo "\n hello1 v2:"
-k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello1-svc:8001/hello/eric
-echo
-k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello1-svc:8001/hello/eric
-echo
-k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello1-svc:8001/hello/eric
-echo "\n hello2 v2:"
-k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello2-svc:8001/hello/eric
-echo
-k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello2-svc:8001/hello/eric
-echo
-k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello2-svc:8001/hello/eric
-echo "\n hello3 v2:"
-k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello3-svc:8001/hello/eric
-echo
-k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello3-svc:8001/hello/eric
-echo
-k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello3-svc:8001/hello/eric
+  echo "\n hello1 v2:"
+  k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello1-svc:8001/hello/eric
+  echo
+  k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello1-svc:8001/hello/eric
+  echo
+  k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello1-svc:8001/hello/eric
+  echo "\n hello2 v2:"
+  k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello2-svc:8001/hello/eric
+  echo
+  k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello2-svc:8001/hello/eric
+  echo
+  k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello2-svc:8001/hello/eric
+  echo "\n hello3 v2:"
+  k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello3-svc:8001/hello/eric
+  echo
+  k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello3-svc:8001/hello/eric
+  echo
+  k -n http-hello exec "$hello3_v2_pod" -c hello-v2-deploy -- curl -s -H "version:v2" hello3-svc:8001/hello/eric
+}
